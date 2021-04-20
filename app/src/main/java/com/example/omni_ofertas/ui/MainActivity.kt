a85359899a83bc39bcd25c4db651636f3eed4e47
José Antoio <afana500@gmail.com>
2021-04-20 18:52:40 +0200

    Añadidos activity_registro y  estilos

--- app/src/main/java/com/example/omni_ofertas/ui/MainActivity.kt
+++ app/src/main/java/com/example/omni_ofertas/ui/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.omni_ofertas.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.omni_ofertas.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btRegistro_login.setOnClickListener {
+            val intent = Intent(this,RegistroActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
     }
+
+
+
+
+
 }
