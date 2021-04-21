abc84a7449ff9c70e4c994a1aac96af4219ee6cb
José Antoio <afana500@gmail.com>
2021-04-21 12:17:37 +0200

    Metodo de cambio de activity actualizado

--- app/src/main/java/com/example/omni_ofertas/ui/MainActivity.kt
+++ app/src/main/java/com/example/omni_ofertas/ui/MainActivity.kt
@@ -9,17 +9,29 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btRegistro_login.setOnClickListener {
-            val intent = Intent(this,RegistroActivity::class.java)
-            startActivity(intent)
+        btRegistro_login.setOnClickListener(this)
+
+
+
+
+    }
+
+    override fun onClick(view: View?) {
+        when(view){
+            btRegistro_login ->accederRegistro()
 
         }
+    }
 
 
+    private fun accederRegistro(){
+
+        val intent = Intent(this,RegistroActivity::class.java)
+        startActivity(intent)
     }
 
 
