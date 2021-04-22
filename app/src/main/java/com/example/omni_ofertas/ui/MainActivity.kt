6541e6279ae82caaf091faecb443d7df3b170fb2
José Antoio <afana500@gmail.com>
2021-04-22 16:54:42 +0200

    Realizada conexion con firebase

--- app/src/main/java/com/example/omni_ofertas/ui/MainActivity.kt
+++ app/src/main/java/com/example/omni_ofertas/ui/MainActivity.kt
@@ -5,7 +5,10 @@ import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import com.example.omni_ofertas.R
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -15,8 +18,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        btRegistro_login.setOnClickListener(this)
 
+        btRegistro_login.setOnClickListener(this)
+        btEntrar_login.setOnClickListener(this)
+
+
+        //Google analytics
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle();
+        bundle.putString("message","Integración de Firebbase completa")
+        analytics.logEvent("InitScreen",bundle)
 
 
 
@@ -25,7 +36,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(view: View?) {
         when(view){
             btRegistro_login ->accederRegistro()
-
+            btEntrar_login -> signUp()
         }
     }
 
@@ -36,8 +47,33 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         startActivity(intent)
     }
 
+    private fun signUp(){
+
+        if (edEmail_login.text.isNotEmpty() && edPass_login.text.isNotEmpty()){
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(edEmail_login.text.toString()
+                ,edPass_login.text.toString()).addOnCompleteListener{
+
+                if (it.isSuccessful){
+
+                }else{
+                    errorAutentificacion()
+                }
+
+            }
+        }
+
+    }
 
 
+    private fun errorAutentificacion(){
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autentificando al usuario")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
 
 
 }
